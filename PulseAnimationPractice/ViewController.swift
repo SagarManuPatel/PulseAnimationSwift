8126c3622738ece3c1dbec543025b014e69b83d1
Allied Infosoft <alliedinfosoft@Allied-Infosofts-MacBook-Pro.local>
2017-06-20 10:58:02 +0530

    Initial Commit

---
+++ PulseAnimationPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PulseAnimationPractice
+//
+//  Created by Allied Infosoft on 20/06/17.
+//  Copyright © 2017 Allied Infosoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
